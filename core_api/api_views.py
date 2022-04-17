505120e7f2456db978b91496b0c459061233a8d8
Acetonee <acetonee@connect.hku.hk>
2022-04-17 16:32:25 +0800

    Added views by hku_id that displays the venues visited

--- core_api/api_views.py
+++ core_api/api_views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+import datetime
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Venue, HKUMember, Entry, Exit
 from .serializers import VenueSerializer, HKUMemberSerializer, EntrySerializer, ExitSerializer
 
@@ -7,6 +10,12 @@ class VenueViewSet(viewsets.ModelViewSet):
     serializer_class = VenueSerializer
     lookup_field = 'venue_code'
     lookup_value_regex = '[^/]+'
+
+    @action(detail=True, url_path='(?P<date>[^/]+)')
+    def get_with_objectid(self, request, venue_code=None, date=None):
+        queryset = Venue.objects.filter(exit__exit_hku_id__hku_id=venue_code).filter(exit__exit_date__range=[(datetime.datetime.strptime(date, "%Y-%m-%d") - datetime.timedelta(days=2)).strftime("%Y-%m-%d"), date]).only("venue_code")
+        serializer = VenueSerializer(queryset, many=True)
+        return Response([i["venue_code"] for i in serializer.data], status=status.HTTP_200_OK)
 
 class HKUMemberViewSet(viewsets.ModelViewSet):
     queryset = HKUMember.objects.all()
