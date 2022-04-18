303842fcc5f259736f887e0df0ccf0415ca397ed
KevinC008 <72290176+KevinC008@users.noreply.github.com>
2022-04-18 19:18:21 +0800

    minor changes

--- core_api/api_views.py
+++ core_api/api_views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Venue, HKUMember, Entry, Exit
+from .models import *
 from .serializers import *
 import datetime
 
